f4695a1a4360cb2b575974661d88125a72da76e2
Hamza Carew <hamzacarew@gmail.com>
2020-05-23 19:49:38 +0100

    Added command to lookup name services for domain

--- cmd/commands.go
+++ cmd/commands.go
@@ -6,16 +6,16 @@ import (
 	"log"
 )
 
-// LookUpIP - Returns info about an IP address
+// LookUpDomain - Returns IP for domain
 func LookUpDomain() *cobra.Command {
 	return &cobra.Command{
-		Use: "Domain", 
+		Use: 		"domain",
+		ValidArgs:  []string{"www.google.com"},
 		RunE: func(cmd *cobra.Command, args[] string) error {
-			res, err := net.LookupIP(args[0])
+			domain := args[0]
+			res, err := net.LookupIP(domain)
 
-			if err != nil {
-				log.Println(err)
-			}
+			if len(res) == 0 { log.Println(err) }
 
 			cmd.Println(args[0] + " : {")
 			cmd.Println("IPv6 Address: ", res[0])
@@ -26,3 +26,26 @@ func LookUpDomain() *cobra.Command {
 		},
 	}
 }
+
+// LookUpNameServices - Looks up the name services for a particular domain
+func LookUpNameServices() *cobra.Command {
+	return &cobra.Command{
+		Use: "ns", 
+		RunE: func(cmd *cobra.Command, args[] string) error {
+			domain := args[0]
+			res, _ := net.LookupNS(domain)
+
+			if len(res) == 0 { log.Println("Failed to lookup name services for: ", domain) }
+
+			for _, nameService := range  res {
+				cmd.Printf("%s\n", nameService.Host)
+			}
+			return nil // Return nil as there were no errors.
+		},
+	}
+}
+
+
+
+
+
--- main.go
+++ main.go
@@ -8,14 +8,17 @@ import (
 )
 
 func main() {
+	// Parent command for program
 	cmd := &cobra.Command{
-		Use:	 "lookup",
+		Use:	 "./Lookup-CLI",
 		Short: 	 "A CLI network lookup tool",
 		Long: 	 `A command line network tool written in Go that can be used 
 		to resolve information. `,
 	}
 
+	// Attach subcommands to main parent command
 	cmd.AddCommand(comm.LookUpDomain())
+	cmd.AddCommand(comm.LookUpNameServices())
 
 	if err := cmd.Execute(); err != nil {
 		log.Println(err)
